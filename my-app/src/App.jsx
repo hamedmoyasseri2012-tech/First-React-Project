259fcb4a58afcb45434ba8be47b23bb44f6c89d9
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-07 03:49:13 +0330

    v2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="flex flex-col h-screen items-center justify-center bg-[#e6e6e6] gap-4 ">
       <h1 className="text-4xl font-bold text-[#000a1c]">React Counter</h1>
       <div className="flex flex-col bg-white w-162.5 h-64 rounded-xl justify-center items-center gap-8">
-        <span className="text-3xl">0</span>
+        <span className="text-3xl">{count}</span>
         <div className="flex gap-2 justify-center items-center">
           <button
             onClick={() => {
@@ -44,7 +44,7 @@ const App = () => {
           </button>
         </div>
         <button
-          nClick={() => setcount(0)}
+          onClick={() => setcount(0)}
           className="w-32.5 h-12.5 border rounded-xl bg-[#f2f2f2] border-[#dddddd]"
         >
           🔄 Reset
